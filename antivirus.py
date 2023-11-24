f6b56371e47c3121f8fef9a1bada3eff18cafcf2
StefanDG1 <danistefangheorghiu@gmail.com>
2023-11-24 19:20:49 +0100

    refactored code to be prettier and added comments

--- antivirus.py
+++ antivirus.py
@@ -3,7 +3,6 @@ from os import path, walk, remove
 from os.path import *
 import hashlib
 import requests
-import json
 
 
 # initializing the customtkinter app window and giving it dimensions and a title
@@ -13,20 +12,55 @@ app = CTk()
 app.geometry(f"{width}x{height}")
 app.title("Antivirus")
 
-# creating an empty string and tuple where the dir and filenames will be stored
+# initializing the empty variables that will be needed later
 dirname = ''
 filenames = ()
-dir_selected = 0
 file_list = []
 hash_list = []
 bad_files_index = []
+file_selection = []
+results_list = []
+info_dict = {}
+file_labels = []
+bad_file_labels = []
+
+
+# reset function
+def reset_all():
+    global dirname
+    global filenames
+    global file_list
+    global hash_list
+    global bad_files_index
+    global file_selection
+    global results_list
+    global info_dict
+    global file_labels
+    global bad_file_labels
+    if (len(file_labels) > 0):
+        for label in file_labels:
+            label.destroy()
+    if (len(bad_file_labels) > 0):
+        for label in bad_file_labels:
+            label.destroy()
+    info_label.configure(text='')
+
+    
+    dirname = ''
+    filenames = ()
+    file_list = []
+    hash_list = []
+    bad_files_index = []
+    file_selection = []
+    results_list = []
+    info_dict = {}
+    file_labels = []
+    bad_file_labels = []
 
 
 # function to make a list with the absolute paths of all needed files
 def get_file_list():
     global file_list
-    file_list = []
-    print(dirname)
     if dirname != '':
         # use the os.walk() function to get the file names in all subdirectories 
         for root, dirs, files in walk(os.path.abspath(dirname)):
@@ -37,7 +71,6 @@ def get_file_list():
 
 # inspiration from https://howtodoinjava.com/python-modules/python-find-file-hash/
 def hash_files():
-    
     global hash_list
     hash_list = []
     # iterate through every filename in the file list
@@ -53,8 +86,8 @@ def hash_files():
                 md5_h.update(chunk)
         # get the hex digest and append it to the hash list
         hash_list.append(md5_h.hexdigest())
-    print(hash_list)
 
+# open the md5 hash file and compare hashes to hashes from file, if found add to the bad files list
 def virus_scan():
     global bad_files_index
     bad_files_index = []
@@ -64,14 +97,8 @@ def virus_scan():
             if(hash in hashes):
                 bad_files_index.append(i)
 
-    print(bad_files_index)
-    for index in bad_files_index:
-        print(file_list[index])
-
-results_list = []
-info_dict = {}
+# run scan function that runs other functions as needed
 def run_scan():
-    #get_file_list()
     hash_files()
     virus_scan()
     global info_dict
@@ -81,12 +108,9 @@ def run_scan():
             result = virusTotal_scan(hash, i)
             results_list.append(result)
         info_dict = {bad_files_index[i]: results_list[i] for i in range(len(bad_files_index))}
-    print(bad_files_index)
-    for index in bad_files_index:
-        print(file_list[index])
     display_bad_files(bad_files_index)
 
-
+# create labels to display the path of the files on the screen
 def display_files():
     global file_labels
     get_file_list()
@@ -96,35 +120,32 @@ def display_files():
         file_labels[i] = CTkLabel(scrollable_frame_top, text=file_list[i], fg_color="#3B8ED0", corner_radius=8, padx=10)
         file_labels[i].pack(side="top")
 
-def clear_files():
-    if (len(file_labels) > 0):
-        for label in file_labels:
-            label.destroy()
-    if (len(bad_file_labels) > 0):
-        for label in bad_file_labels:
-            label.destroy()
+# create clickable buttons (with the selected() lambda function to be able to pass an argument 
+# and only call it when it is clicked) to display the malicious files
+def display_bad_files(bad_indexes):
+    global bad_file_labels
+    bad_file_labels = [None] * len(bad_indexes)
 
-file_labels = []
+    for i in range(len(bad_file_labels)):
+        bad_file_labels[i] = CTkButton(scrollable_frame_bottom, text=file_list[i], command= lambda id=i: selected(id), hover_color="#c70000", fg_color="#3B8ED0")
+        bad_file_labels[i].pack(side="top")
+
 # function to open the dialog box to select a directory and stores it in the dirname string
 def dir_dialog():
-    clear_files()
+    reset_all()
     global dirname
-    global filenames
-    filenames = ()
     dirname = filedialog.askdirectory()
     display_files()
 
 
 # function to open the dialog box to select filenames and stores it in the dirname tuple
 def filenames_dialog():
-    clear_files()
-    global dirname
+    reset_all()
     global filenames
-    dirname = ''
     filenames = filedialog.askopenfilenames()
     display_files()
 
-
+# use the VirusTotal api to extract data from a hash and return the result in a dictionary
 def virusTotal_scan(id, i):
     url = f"https://www.virustotal.com/api/v3/files/{id}"
     api_key = "8da3c1b86b591198d304fbb6ee71f586057cef49ca8065cb3d9d2e2144e4e40a"
@@ -159,7 +180,6 @@ def virusTotal_scan(id, i):
                 bad_files_index.append(i)
         else: analysis_result = "low"
 
-        print(f"The name of the malware is {name}, {community_score_harmless} people from the community scored it as harmless while {community_score_malicious} scored it as malicious. From the last analysis, this many antiviruses flagged it as: {an_harmless} harmless, {an_malicious} malicious, {an_undetected} undetected.\n\tThe result of the scan is {analysis_result}")
         result = {
             "name": name,
             "community_score_harmless": community_score_harmless,
@@ -173,37 +193,28 @@ def virusTotal_scan(id, i):
     except:
         return "error"
 
-
+# remove all malicious files
 def remove_files():
     for index in bad_files_index:
         os.remove(file_list[index])
-    clear_files()
+    reset_all()
 
+# remove selected files
 def remove_selection():
-    print(file_selection)
     for file in file_selection:
         os.remove(file)
-    clear_files()
+    reset_all()
 
 
-bad_file_labels = []
-def display_bad_files(bad_indexes):
-    global bad_file_labels
-    bad_file_labels = [None] * len(bad_indexes)
 
-    for i in range(len(bad_file_labels)):
-        bad_file_labels[i] = CTkButton(scrollable_frame_bottom, text=file_list[i], command= lambda idx=i: selected(idx), hover_color="#c70000", fg_color="#3B8ED0")
-        bad_file_labels[i].pack(side="top")
-
+# use the dictionary with results to change the info text
 def change_info_text(index):
-    info_data = info_dict[index]
-    info_text = f"The name of the file is {info_data['name']}.\nThe community score is: \
-                {info_data['community_score_harmless']} as harmless and {info_data['community_score_malicious']} \
-                as malicious.\nThis many antiviruses scored it as: harmless: {info_data['analysis_harmless']}\
-                 | malicious {info_data['analysis_malicious']} | undetected {info_data['analysis_undetected']} \nThe estimated danger level is: {info_data['analysis_result']}"
-    info_label.configure(text=info_text)
+    if (info_dict[index] != "error"):
+        info_data = info_dict[index]
+        info_text = f"Details of file:\nThe name of the file is {info_data['name']}.\nThe community score is: {info_data['community_score_harmless']} as harmless and {info_data['community_score_malicious']} as malicious.\nThis many antiviruses scored it as: harmless {info_data['analysis_harmless']} | malicious {info_data['analysis_malicious']} | undetected {info_data['analysis_undetected']} \nThe estimated danger level is: {info_data['analysis_result']}"
+        info_label.configure(text=info_text)
 
-file_selection = []
+# change colors and add/remove file toggle
 def selected(num):
     global file_selection
     label_obj = bad_file_labels[num]
@@ -217,9 +228,9 @@ def selected(num):
         file_selection.remove(text) 
         label_obj.configure(fg_color="#3B8ED0")
         label_obj.configure(hover_color="#c70000")
-    print(file_selection)
 
-# top frame where buttons are placed
+
+# left frame where buttons are placed
 container_left = CTkFrame(master=app, fg_color="#F6F4EB", width=150, corner_radius=0)
 container_left.pack(side="left", fill="y")
 container_left.pack_propagate(False)
@@ -239,40 +250,44 @@ run_btn.place(rely=0.45, relx=0.5, anchor="center")
 
 delete_btn = CTkButton(container_left, text="Delete Selected Files", command=remove_selection, fg_color="#c70000", hover_color="#990000")
 delete_btn.place(rely=0.85, relx=0.5, anchor="center")
-delete_all_btn = CTkButton(container_left, text="Delete all files", command=remove_files)
+delete_all_btn = CTkButton(container_left, text="Delete all files", command=remove_files, fg_color="#c70000", hover_color="#990000")
 delete_all_btn.place(rely=0.91, relx=0.5, anchor="center")
 
 
 
-# add and remove files in the top frame
+# right frame split into 2 parts, top and bottom
 container_right = CTkFrame(master=app, corner_radius=0)
 container_right.pack(side="right", expand=True, fill="both")
 
+# top part
 frame_top = CTkFrame(master=container_right, corner_radius=0)
 frame_top.pack(side="top", expand=True, fill="both")
 frame_top.pack_propagate(False)
 
+# scrollable frame in top frame
 scrollable_frame_top = CTkScrollableFrame(master=frame_top, fg_color="#749BC2", border_color="#F6F4EB", border_width=1, corner_radius=0)
 scrollable_frame_top.pack(expand=True, fill="both")
 top_label = CTkLabel(master=scrollable_frame_top, text="Selected files")
 top_label.pack(side="top")
 
+# bottom part
 frame_bottom = CTkFrame(master=container_right)
 frame_bottom.pack(side="bottom", expand=True, fill="both")
 frame_bottom.pack_propagate(False)
 
+# scrollable bottom frame
 scrollable_frame_bottom = CTkScrollableFrame(master=frame_bottom, fg_color="#4682A9", border_color="#F6F4EB", border_width=1, corner_radius=0)
 scrollable_frame_bottom.pack(side="top", fill="both", expand=True)
 bottom_label = CTkLabel(master=scrollable_frame_bottom, text="Malicious files")
 bottom_label.pack(side="top")
 
+# info section
 frame_info = CTkFrame(master=frame_bottom, fg_color="#3B8ED0", border_color="#F6F4EB", border_width=1, corner_radius=0)
 frame_info.pack(side="bottom", fill="both", expand=True)
 info_sub_frame = CTkFrame(master=frame_info, fg_color="#3B8ED0")
 info_sub_frame.pack(side="left", fill="both", expand=True)
 info_label = CTkLabel(master=info_sub_frame, padx=30, pady=20, fg_color="transparent", text="The details of the malicious files is:")
 info_label.pack(side="top")
-
 
 
 app.mainloop()
